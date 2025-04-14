632853f2825e9532a890120181035737cd5d1c4d
Menna ayman <mennaaymangalal4@gmail.com>
2025-04-15 00:54:35 +0200

    update

--- src/pages/Categorie/Categorie.jsx
+++ src/pages/Categorie/Categorie.jsx
@@ -9,7 +9,7 @@ export default function Categorie({product}) {
             <div className="flex align-middle justify-center">
               <img  src={product.strMealThumb} alt="" className='group-hover:rotate-360 transition-all duration-700 rounded-full w-40 h-40 shadow-2xl  -mt-20'/>
             </div>
-            <div className="flex flex-col items-center ">
+            <div className="flex flex-col items-center align-middle justify-center ">
               <h3 className=' pt-6 font-bold text-2xl font-pacifico line-clamp-4 min-h-40'>{product.strMeal}</h3>              
               <Button className='px-5 py-8 font-pacifico mb-6 bg-green-500 text-white text-lg font-bold rounded-full '>
                 <Link  to={"/mealdetails/" + product.idMeal} >View Recipe</Link>
